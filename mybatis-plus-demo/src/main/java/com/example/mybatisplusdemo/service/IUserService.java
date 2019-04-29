bae209a73d5fa3707bffd322e881defdcd925d51
wangyoushun <wys666666>
2019-04-29 14:23:15 +0800

    add

---
+++ mybatis-plus-demo/src/main/java/com/example/mybatisplusdemo/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.example.mybatisplusdemo.service;
+
+import java.util.List;
+
+import com.example.mybatisplusdemo.bean.User;
+
+public interface IUserService {
+	List<User> pageList();
+}
